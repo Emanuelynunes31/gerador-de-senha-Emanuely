53686ca307e2021c031e5ee2aef763fa04173d27
Emanuelynunes31 <108131108+Emanuelynunes31@users.noreply.github.com>
2024-04-10 11:06:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,55 @@ function aumentaTamanho(){
     tamanhoSenha = tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
 }
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+         //tamanhoSenha = tamanhoSenha-1;
+         tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       //tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox')
+for (i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?'
+campoSenha.value = letrasMaiusculas;
+geraSenha();
+let alfabeto = '';
+console.log(alfabeto);
+function geraSenha(){
+     let senha = '' ;
+    if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
+if (checkbox[1].checked){
+            alfabeto = alfabeto + letrasMinusculas;
+        }
+}
+if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+}
+if (checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
+}
+
+   for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
+}
+
