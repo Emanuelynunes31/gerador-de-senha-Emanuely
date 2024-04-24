799a11f3a42b05a8af935cd5bed2935b71c10904
Emanuelynunes31 <108131108+Emanuelynunes31@users.noreply.github.com>
2024-04-24 11:01:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,89 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
-console.log(botoes)
-
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
-
-function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
-    numeroSenha.textContent = tamanhoSenha;
-}
-function aumentaTamanho(){
-    tamanhoSenha = tamanhoSenha+1;
-    numeroSenha.textContent = tamanhoSenha;
-}
-function diminuiTamanho(){
-    if (tamanhoSenha > 1){
-         //tamanhoSenha = tamanhoSenha-1;
-         tamanhoSenha--;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}
-function aumentaTamanho(){
-    if (tamanhoSenha < 20){
-       //tamanhoSenha = tamanhoSenha+1;
-       tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-console.log(checkbox[0].checked);
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
-campoSenha.value = letrasMaiusculas ;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
 
 geraSenha();
-let alfabeto = '';
-if (checkbox[0].checked){
-    alfabeto = alfabeto + letrasMaiusculas;
-}
-console.log(alfabeto);
 
-function geraSenha(){
-    let senha = '' ;
-    for (let i = 0; i < tamanhoSenha;i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-       console.log(letrasMaiusculas[numeroAleatorio]);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+       classificaSenha(alfabeto.length);
 }
-campoSenha.value = senha;
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto); 
+    console.log(entropia);
+     forcaSenha.classList.remove('fraca','media','forte');
+        if (entropia > 57){
+            forcaSenha.classList.add('forte');
+        } else if (entropia > 35 && entropia < 57) {
+            forcaSenha.classList.add('media');
+        } else if (entropia <= 35){
+                forcaSenha.classList.add('fraca');
+    }
+
+
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+    }
 }
+
+
+
+
+
 
